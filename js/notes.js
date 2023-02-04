f2e1189b19440f1184afa4ec36103ab368d66e50
XiaoxingChen <chen-xx@zju.edu.cn>
2023-02-04 18:45:45 +0800

    feature: enable featured heading in notes.js.

--- js/notes.js
+++ js/notes.js
@@ -1,5 +1,9 @@
 import {basicHeadSetting} from './note_basic.js'
 import {createTableOfContents} from './table_of_contents.js'
+import {assignHeadingFeatures} from './heading_feature.js'
+
+let max_featured_heading_level = 3
 
 basicHeadSetting()
-createTableOfContents(3)
+assignHeadingFeatures(max_featured_heading_level)
+createTableOfContents(max_featured_heading_level)
